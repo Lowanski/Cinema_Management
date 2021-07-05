439440390854ad6026e1ec4802c1d0eedb0eac76
Lowan Steinmetz <lowan.steinmetz@edu.ece.fr>
2021-07-05 13:19:49 +0200

    Start created FrameSignIn.fxml view

--- src/ece/fr/Controller/SignIn.java
+++ src/ece/fr/Controller/SignIn.java
@@ -1,11 +1,44 @@
 package ece.fr.Controller;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-public class SignIn extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class SignIn {
 
-    }
+    @FXML
+    private Button BUquit;
+
+    @FXML
+    private Label LApassword;
+
+    @FXML
+    private Label LAemail;
+
+    @FXML
+    private TextField TFemailinput;
+
+    @FXML
+    private TextField TFpasswordinput;
+
+    @FXML
+    private Label LAsignin;
+
+    @FXML
+    private Button BUcreateaccount;
+
+    @FXML
+    private Button BUvalidate;
+
+    @FXML
+    private Button BUsigninguest;
+
+    @FXML
+    private ImageView IVimageaccueil;
+
+    @FXML
+    private Label LAgrospathe;
+
 }
