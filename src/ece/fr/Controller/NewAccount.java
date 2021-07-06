c63a064c798dd65e146bed50027e18375d597080
Lowan Steinmetz <lowan.steinmetz@edu.ece.fr>
2021-07-06 13:04:51 +0200

    New account frame created

---
+++ src/ece/fr/Controller/NewAccount.java
@@ -0,0 +1,13 @@
+package ece.fr.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewAccount implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
