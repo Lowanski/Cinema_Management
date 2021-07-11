c027c2bd87c45c232ba3381428cb24b2b0cca083
Lowan Steinmetz <lowan.steinmetz@edu.ece.fr>
2021-07-11 17:18:25 +0200

    Formular finish

--- src/ece/fr/Model/User.java
+++ src/ece/fr/Model/User.java
@@ -1,5 +1,16 @@
 package ece.fr.Model;
 
+/**
+ *
+ * User type :
+ * 0 = Guest
+ * 1 = Children
+ * 2 = Regular
+ * 3 = Senior
+ * 5 = Employee
+ *
+ */
+
 public class User {
     protected int userID;
 
