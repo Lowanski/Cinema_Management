6cae7a62bcbcaa916b5206dd09a953bf3cce9423
Lowan Steinmetz <lowan.steinmetz@edu.ece.fr>
2021-07-21 21:45:38 +0200

    Film poster added

--- src/ece/fr/Controller/Booking.java
+++ src/ece/fr/Controller/Booking.java
@@ -1,11 +1,59 @@
 package ece.fr.Controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class Booking extends Application {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class Booking implements Initializable {
+    @FXML
+    private ImageView IWfilm;
+
+    @FXML
+    private Label LAfilm;
+
+    @FXML
+    private ChoiceBox<?> CBday;
+
+    @FXML
+    private ChoiceBox<?> CBhour;
+
+    @FXML
+    private Button BUbuy;
+
+    @FXML
+    private Button BUback;
+
+    @FXML
+    void handleButtonActionBUback(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonActionBUbuy(ActionEvent event) throws IOException {
+        Parent home = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("ece/fr/View/FramePayment.fxml")));
+        Scene scene = new Scene(home);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
--- src/ece/fr/Model/AuthentificatedUser.java
+++ src/ece/fr/Model/AuthentificatedUser.java
@@ -43,4 +43,6 @@ public class AuthentificatedUser extends User{
     public String getPassword() {
         return password;
     }
+
+    public String getGender() { return gender; }
 }
